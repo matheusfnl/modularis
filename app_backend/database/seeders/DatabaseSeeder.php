3db430d79cc92fca7a0bc17c4039e669d023d5ba
matheusfnl <matheusggco01@gmail.com>
2024-12-07 21:43:28 -0300

    fix disabled

--- app_backend/database/seeders/DatabaseSeeder.php
+++ app_backend/database/seeders/DatabaseSeeder.php
@@ -9,7 +9,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserSeeder::class,
+            // UserSeeder::class,
             ModuleSeeder::class,
         ]);
     }
